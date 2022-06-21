a535c6f6492df29b629536d44253f2ed52076613
gabrielalvito <alvitoadinugraha@gmail.com>
2022-06-21 21:37:02 +0700

    add Activity

--- app/src/main/java/com/example/retrofit/Activity/tambah.java
+++ app/src/main/java/com/example/retrofit/Activity/tambah.java
@@ -3,14 +3,76 @@ package com.example.retrofit.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.retrofit.API.APIRequestData;
+import com.example.retrofit.API.RetroServer;
+import com.example.retrofit.Model.ResponseModel;
 import com.example.retrofit.R;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class tambah extends AppCompatActivity {
+    private EditText etNama, etNim, etKelas;
+    private Button btnSimpan;
+    private String nama, nim, kelas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah);
+
+        etNama = findViewById(R.id.et_nama);
+        etNim = findViewById(R.id.et_nim);
+        etKelas = findViewById(R.id.et_kelas);
+        btnSimpan = findViewById(R.id.btn_simpan);
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nama = etNama.getText().toString();
+                nim = etNim.getText().toString();
+                kelas = etKelas.getText().toString();
+
+                if(nama.trim().equals("")){
+                    etNama.setError("Nama Harus Diisi");
+                }
+                else if(nim.trim().equals("")){
+                    etNim.setError("Nim Harus Diisi");
+                }
+                else if(kelas.trim().equals("")){
+                    etKelas.setError("Kelas Harus Diisi");
+                }
+                else{
+                    createData();
+                }
+            }
+        });
+    }
+
+    private void createData(){
+        APIRequestData ardData = RetroServer.konekRetrofit().create(APIRequestData.class);
+        Call<ResponseModel> simpanData = ardData.ardCreateData(nama, nim, kelas);
+
+        simpanData.enqueue(new Callback<ResponseModel>() {
+            @Override
+            public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
+                int kode = response.body().getKode();
+                String pesan = response.body().getPesan();
+
+                Toast.makeText(tambah.this, "Kode : "+kode+" | Pesan : "+pesan, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseModel> call, Throwable t) {
+                Toast.makeText(tambah.this, "Gagal Menghubungi Server | "+t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/retrofit/Activity/ubah.java
+++ app/src/main/java/com/example/retrofit/Activity/ubah.java
@@ -2,15 +2,80 @@ package com.example.retrofit.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.retrofit.API.APIRequestData;
+import com.example.retrofit.API.RetroServer;
+import com.example.retrofit.Model.ResponseModel;
 import com.example.retrofit.R;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ubah extends AppCompatActivity {
+    private int xId;
+    private String xNama, xNim, xKelas;
+    private EditText etNama, etNim, etKelas;
+    private Button btnUbah;
+    private String yNama, yNim, yKelas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ubah);
+
+        Intent terima = getIntent();
+        xId = terima.getIntExtra("xId", -1);
+        xNama = terima.getStringExtra("xNama");
+        xNim = terima.getStringExtra("xNim");
+        xKelas = terima.getStringExtra("xKelas");
+
+        etNama = findViewById(R.id.et_nama);
+        etNim = findViewById(R.id.et_nim);
+        etKelas = findViewById(R.id.et_kelas);
+        btnUbah = findViewById(R.id.btn_ubah);
+
+        etNama.setText(xNama);
+        etNim.setText(xNim);
+        etKelas.setText(xKelas);
+
+        btnUbah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                yNama = etNama.getText().toString();
+                yNim = etNim.getText().toString();
+                yKelas = etKelas.getText().toString();
+
+                updateData();
+            }
+        });
+    }
+
+    private void updateData(){
+        APIRequestData ardData = RetroServer.konekRetrofit().create(APIRequestData.class);
+        Call<ResponseModel> ubahData = ardData.ardUpdateData(xId, yNama, yNim, yKelas);
+
+        ubahData.enqueue(new Callback<ResponseModel>() {
+            @Override
+            public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
+                int kode = response.body().getKode();
+                String pesan = response.body().getPesan();
+
+                Toast.makeText(ubah.this, "Kode : "+kode+" | Pesan : "+pesan, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseModel> call, Throwable t) {
+                Toast.makeText(ubah.this, "Gagal Menghubungi Server | "+t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

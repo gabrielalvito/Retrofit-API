eccb49179ed891d5103e68b1ee85b25d719445dc
gabrielalvito <alvitoadinugraha@gmail.com>
2022-06-21 21:12:55 +0700

    update layout activity

--- app/src/main/java/com/example/retrofit/tambah.java
+++ app/src/main/java/com/example/retrofit/tambah.java
@@ -1,6 +1,14 @@
 package com.example.retrofit;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class tambah extends Activity {
+import android.os.Bundle;
+
+public class tambah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah);
+    }
 }
--- app/src/main/java/com/example/retrofit/ubah.java
+++ app/src/main/java/com/example/retrofit/ubah.java
@@ -1,6 +1,14 @@
 package com.example.retrofit;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class ubah extends Activity {
+import android.os.Bundle;
+
+public class ubah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah);
+    }
 }
